37052563a0bf26dfa97d32c02bbd9907ae9208ad
orc <lalanne@gmail.com>
2017-05-29 16:19:44 -0400

    ADD: personal dev env

--- Legion/scripts/personal_dev.sh
+++ Legion/scripts/personal_dev.sh
@@ -2,7 +2,7 @@
 
 function personal_dev_setup {
     echo "Here we should put personal configurations of vim, tmux, ..etc"
-    #sudo yum -y install zsh
+    sudo apt-get --assume-yes install zsh
 
     git clone git@github.com:lalanne/ConfigurationFiles.git
     git clone git@github.com:lalanne/PersonalScripts.git
@@ -10,12 +10,12 @@ function personal_dev_setup {
     ln -s ConfigurationFiles/vimrc .vimrc
     ln -s ConfigurationFiles/tmux.conf .tmux.conf
 
-    #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     mkdir -p .vim/colors/
     cp /vagrant/molokai.vim ~/.vim/colors/
 
-    #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 
