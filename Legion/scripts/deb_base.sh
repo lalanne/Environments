2919e5e733a6d0a4485be74736e6b51d49e3219b
orc <lalanne@gmail.com>
2017-05-30 09:10:57 -0400

    ADD: g++

--- Legion/scripts/deb_base.sh
+++ Legion/scripts/deb_base.sh
@@ -2,6 +2,7 @@
 
 function install_deb_base {
     sudo apt-get update 
+    sudo apt-get --assume-yes install g++
     sudo apt-get --assume-yes install git
     sudo apt-get --assume-yes install cmake
 }
